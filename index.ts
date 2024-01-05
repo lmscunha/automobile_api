813f9f3105e03f916d1efa9208f8cda562dc35bc
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-01-05 20:30:27 -0300

    update default port

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
 import { app } from "./src/server";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
